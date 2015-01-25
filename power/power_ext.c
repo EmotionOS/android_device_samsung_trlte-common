39f8d3df50a5a2f0b332f57e466e7a8f285f3534
Tony Layher <layhertony@gmail.com>
2015-01-25 13:10:13 -0500

    trlte-common: Update perms and power hal for changes in input devices.
    Change-Id: If806565966e143a58fc28aad2aca20ae9e201ba3

--- power/power_ext.c
+++ power/power_ext.c
@@ -22,7 +22,7 @@
 #include <utils/Log.h>
 
 #define TOUCHKEY_POWER "/sys/class/input/input1/enabled"
-#define SPEN_POWER "/sys/class/input/input20/enabled"
+#define SPEN_POWER "/sys/class/input/input18/enabled"
 #define TSP_POWER "/sys/class/input/input2/enabled"
 #define LCD_POWER "/sys/class/power_supply/battery/lcd"
 
