b5c23d59c2af0b5989791767df2f1719713de09a
Tony Layher <layhertony@gmail.com>
2015-02-14 12:06:39 -0500

    trlte-common: Add NFC property
      Requires http://review.cyanogenmod.org/#/c/88751
    
    Change-Id: I3274266773b6af570fddd8df14dab9c360a230c0

--- system_prop.mk
+++ system_prop.mk
@@ -30,6 +30,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.izat.premium_enabled=0 \
     ro.qc.sdk.izat.service_mask=0x0
 
+# NFC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.nfc.sec_hal=true
+
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.netmgrd.qos.enable=true \
