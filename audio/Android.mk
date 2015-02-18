8c1594789b49851462d3b753daf8d3c3386d330c
gekkehenkie11 <allard_addink@hotmail.com>
2015-02-18 14:57:50 -0400

    change trlteeur -> trltexx
    Change-Id: I95f499e6b03cdc20263e29a777c112b9ac34e179

--- audio/Android.mk
+++ audio/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-ifeq ($(TARGET_DEVICE),trlteeur)
+ifeq ($(TARGET_DEVICE),trltexx)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := mixer_paths.xml
 LOCAL_MODULE_TAGS  := optional
