ea9edf1cfb80a9a60365f2aec0d522b1a44888cb
gekkehenkie11 <allard_addink@hotmail.com>
2015-06-13 00:46:07 -0400

    add izat.conf for gps
    Change-Id: Ie7145d8dbe32e11bbbfb3bbb4a0d67ef2e236638

--- trlte.mk
+++ trlte.mk
@@ -125,6 +125,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/flp.conf:/system/etc/flp.conf \
     $(LOCAL_PATH)/configs/gps.conf:/system/etc/gps.conf \
+    $(LOCAL_PATH)/configs/izat.conf:/system/etc/izat.conf \
     $(LOCAL_PATH)/configs/sap.conf:/system/etc/sap.conf
 
 # Keylayouts
