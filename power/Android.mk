5d58a20899132595a70d32c32ec1f8821e1140e3
fat-tire <f4ttire@gmail.com>
2016-01-20 17:03:56 -0500

    remove redundant line
    Change-Id: Id09487935cf1f69165f5acb47a98aad7045ee91d

--- power/Android.mk
+++ power/Android.mk
@@ -10,8 +10,6 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
 LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c power-8084.c power_ext.c
 
-LOCAL_MODULE := power.apq8084
-
 ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
     LOCAL_CFLAGS += -DINTERACTION_BOOST
 endif
