09dff51ca8097d2c331b5845084ff7c55ebacf4b
ZION959 <ziontran@gmail.com>
2016-01-20 20:17:22 -0700

    L1/L2 cache size parameters by @JustArchi
    Change-Id: Ie15bd9bdeaa4b551f862011bd00e83c2784e89d1
    Signed-off-by: ZION959 <ziontran@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -24,6 +24,9 @@ TARGET_CPU_VARIANT := krait
 
 ENABLE_CPUSETS := true
 
+# L1/L2 cache size parameters by @JustArchi
+BOARD_GLOBAL_CFLAGS := --param l1-cache-size=32 --param l1-cache-line-size=16 --param l2-cache-size=2048
+
 # Audio
 BOARD_USES_ALSA_AUDIO := true
 AUDIO_FEATURE_ENABLED_HWDEP_CAL := true
