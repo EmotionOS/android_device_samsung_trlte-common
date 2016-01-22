a51fed007888e5fd9703ea439772b88b537d4bb0
fat-tire <f4ttire@gmail.com>
2016-01-22 14:29:09 -0500

    add audio/heapsize device settings (from shamu)
    Change-Id: Ic26a359d2cbbbff8cd286ddd4305b2bfc31252ec

--- trlte.mk
+++ trlte.mk
@@ -33,6 +33,9 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
+PRODUCT_PROPERTY_OVERRIDES += \
+   dalvik.vm.heapgrowthlimit=256m
+
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
@@ -261,3 +264,12 @@ PRODUCT_PACKAGES += \
     libcnefeatureconfig \
     libril_shim \
     librmt_shim
+
+# Reduce client buffer size for fast audio output tracks
+PRODUCT_PROPERTY_OVERRIDES += \
+    af.fast_track_multiplier=1
+
+# Low latency audio buffer size in frames
+PRODUCT_PROPERTY_OVERRIDES += \
+    audio_hal.period_size=192
+
