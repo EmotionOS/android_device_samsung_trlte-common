d5d51569d3f929a18c0e82322b6ec5140e3cbe50
Pafcholini <nadyaivanova14@gmail.com>
2016-02-23 02:45:00 +0100

    trlte-common: revert mpdecision stuff
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -53,8 +53,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.core_ctl_min_cpu=2 \
 ro.core_ctl_max_cpu=4 \
-ro.min_freq_0=300000 \
-ro.qualcomm.perf.cores_online=2
+ro.min_freq_0=300000
 
 # lazy dexopt
 PRODUCT_PROPERTY_OVERRIDES += \
