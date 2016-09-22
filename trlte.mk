af658e956ea483559866999e632a2007a4ec307a
Jacob McSwain <jacob.a.mcswain@gmail.com>
2016-09-22 12:03:58 -0500

    Keep sim on
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- trlte.mk
+++ trlte.mk
@@ -34,10 +34,10 @@ PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_PROPERTY_OVERRIDES += 	dalvik.vm.heapgrowthlimit=256m \
+				persist.radio.apm_sim_not_pwdn=1
 
 $(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-dalvik-heap.mk)
-
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxxhdpi-3072-hwui-memory.mk)
 
 # Permissions
