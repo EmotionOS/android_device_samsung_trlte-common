3de55a76dcf2f73f6aef338598b5900db6d7fb36
crimsonthunder <crimsonthunder54@gmail.com>
2017-01-11 15:58:10 +0100

    Enable Android XLAT

--- system_prop.mk
+++ system_prop.mk
@@ -76,6 +76,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.qmi.adb_logmask=0 \
     persist.radio.add_power_save=1 \
     persist.radio.lte_vrat_report=1 \
+    persist.net.doxlat=true \
     ro.telephony.mms_data_profile=5
 
 # Ril
