1073b85f2b53c65c046af23c939a0f08de930c8d
Juha Inkinen <Inksu@users.noreply.github.com>
2017-01-24 12:54:46 +0200

    Whitespace removed

--- system_prop.mk
+++ system_prop.mk
@@ -82,7 +82,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.add_power_save=1 \
     persist.radio.lte_vrat_report=1 \
     persist.radio.apm_sim_not_pwdn=1 \
-    persist.net.doxlat=true \    
+    persist.net.doxlat=true \
     ro.telephony.mms_data_profile=5
 
 # Sensors
